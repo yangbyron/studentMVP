4aacc78046955ee44bc79af312b98e551700e1c0
root <root@LAPTOP-VI6GSBSM.localdomain>
2022-10-10 14:24:12 -0500

    install nodemon, set up port and client connect to psql, set up config.json file for connection string, api get request working

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const {Client}=require('pg');
+const config = require('./config.json')[process.env.NODE_ENV || 'dev'];
+const PORT = 3000;
+const client = new Client({
+    connectionString: config.connectionstring
+});
+client.connect();
+
+app.get('/api/list',(req,res)=>{
+    client.query('SELECT * FROM to_do_list;')
+    .then((data)=>{
+        console.log(data.rows);
+        res.send(data.rows);
+    })
+})
+app.listen(PORT,()=>{
+    console.log('Listening on port', PORT);
+})
